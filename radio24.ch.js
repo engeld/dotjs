a19a498adec34d827b6f35bd1f19a7b49b07ab26
Dana Engel <engel.dana.mail@gmail.com>
2013-10-09 16:09:22 +0200

    remove social-media-elements and ads

---
+++ radio24.ch.js
@@ -0,0 +1,6 @@
+$('.skyscraper_banner_wrap').hide();
+$('.top_banner_wrap').hide();
+$('a.logo').hide();
+$('div.page_main > div.column_content').removeClass('column_content float_left').addClass( "column_full" );
+$('.column_aside').hide();
+
