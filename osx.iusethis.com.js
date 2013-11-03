3ce408cd444996d20b080484450dba836191d189
Dana Engel <engel.dana.mail@gmail.com>
2013-11-03 17:58:59 +0100

    hide unneeded social elements and expand the app-overview table

---
+++ osx.iusethis.com.js
@@ -0,0 +1,7 @@
+$('div#sidebar').hide();
+$('div#maincontent').css( 'width', '890px' );
+$('div.myapps').css( 'width', '890px' );
+$('div.appviewheading').css( 'width', '890px' );
+$('span.titlehead').css( 'width', '767px' );
+$('table.list').css( 'width', '890px' );
+$('.appviewheading').css( 'background', 'url("http://osx.iusethis.com/static/images/listheadbg.gif") repeat scroll 0% 0% transparent' );
